ced4ecff652140f328242ec9dade47f769e0e47c
lcorbitt <movalley5@gmail.com>
2020-10-06 20:16:18 -0600

    Add role enum to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,22 @@ class User < ApplicationRecord
 
   validates_uniqueness_of :email
   validates_uniqueness_of :username
+
+  # Creates the ActiveRecord::Enum mappings between the attribute values and
+  # their associated database integers. Also creates a constant for each value.
+  #
+  # To add a new value, add it to the **end** of the hash, incrementing the integer.
+  #
+  # Do **NOT** remap any existing attributes or integers.
+  #
+  # @see https://api.rubyonrails.org/v5.2/classes/ActiveRecord/Enum.html
+  #
+  ENUM_ROLE_MAPPINGS = {
+    admin: 0,
+    employee: 1
+  }.each_key do |subject|
+    const_set(subject.upcase, subject)
+  end
+
+  enum role: ENUM_ROLE_MAPPINGS
 end
