0fdebdcc6dff34e87fc639d4ddec0cb800fa17c4
lcorbitt <movalley5@gmail.com>
2020-10-06 20:04:29 -0600

    Capture username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true
+  validates :username, presence: true
+
+  validates_uniqueness_of :email
+  validates_uniqueness_of :username
 end
