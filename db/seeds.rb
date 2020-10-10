89b9edb7cf3bc8e400b6dc44b0609311504f7199
lcorbitt <movalley5@gmail.com>
2020-10-10 13:51:01 -0600

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,67 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+###################################################################################
+###########################  Destroy all existing data  ###########################
+###################################################################################
+
+if Rails.env.development?
+  Rake::Task['db:environment:set'].invoke(['RAILS_ENV=development'])
+  Rake::Task['db:drop'].invoke
+  Rake::Task['db:create'].invoke
+  Rake::Task['db:schema:load'].invoke
+end
+
+###################################################################################
+###########################  Create dev user  ###########################
+###################################################################################
+
+
+lukas = User.create(
+  username: "lcorbitt",
+  email: "lukas@example.com",
+  password: "Password1",
+  role: User::ADMIN
+)
+
+###################################################################################
+###########################  Create employees  ###########################
+###################################################################################
+
+10.times do
+  User.create(
+    username: Faker::Internet.username,
+    email: Faker::Internet.email,
+    password: 'Password1',
+    role: User::EMPLOYEE
+  )
+end
+
+###################################################################################
+###########################  Create Projects  ###########################
+###################################################################################
+
+10.times do
+  Project.create(
+    user: lukas,
+    name: Faker::Hipster.sentence,
+    description: Faker::Hipster.sentences.join(' ')
+  )
+end
+
+###################################################################################
+###########################  Create Tickets  ###########################
+###################################################################################
+
+50.times do
+  Ticket.create(
+    user: User.all.sample(),
+    project: Project.all.sample(),
+    title: Faker::Hipster.sentence,
+    description: Faker::Hipster.sentences.join(' '),
+    priority: Ticket.priorities.map { |k,v| v }.sample(),
+    status: Ticket.statuses.map { |k,v| v }.sample()
+  )
+end
+
+puts "#{User.all.size} User records created."
+puts "#{Project.all.size} Project records created."
+puts "#{Ticket.all.size} Ticket records created."
+
