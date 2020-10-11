5ed38652b2ebd90a1a4d13507bbb2eb2bdc19a8b
lcorbitt <movalley5@gmail.com>
2020-10-11 09:57:13 -0600

    Remove unnecessary accepts_nested_attributes_for :tickets

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,6 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :tickets, dependent: :destroy
 
-  accepts_nested_attributes_for :tickets
   validates :name, presence: true
   validates :description, presence: true
 end
