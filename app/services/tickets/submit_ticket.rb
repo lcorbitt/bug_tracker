044b80319e8ae3abf885a8cb73bbc07485696e62
lcorbitt <movalley5@gmail.com>
2020-10-21 11:57:08 -0600

    Ability to update tickets

--- app/services/tickets/submit_ticket.rb
+++ app/services/tickets/submit_ticket.rb
@@ -2,29 +2,35 @@ module Tickets
   class SubmitTicket
     attr_reader :user, :params, :ticket, :errors
 
-    # @param user [User] The User that created the ticket
+    # @param user [User] The User that created or up the ticket
     # @param params [ActionController::Parameters] The params the user submitted when creating the ticket
     # @option params [String] :id The associated Ticket#id - for the update case
-    # @option params [String] :project_id The associated Project#id
+    # @option params [String] :project The associated Project
+    # @option params [String] :title The current Ticket#title
     # @option params [String] :description The current Ticket#description
+    # @option params [String] :status The current Ticket#status
+    # @option params [String] :priority The current Ticket#priority
     #
-    def initialize(user:, project:, params:)
+    def initialize(user:, params:, project:, ticket: nil)
       @user = user
-      @project = project
       @params = params
+      @project = project
+      @ticket = ticket
       @assignee_ids = set_assignee_ids
     end
 
     def save
-      if @project.present?
-        create_ticket_transaction
-        # trigger_notifications
-        success?
-      else
-        @errors = 'Ticket must be assigned to a Project.'
-        false
-      end
-    rescue ArgumentError => e
+      create_ticket_transaction
+      trigger_notifications
+      success?
+    end
+
+    def update
+      update_ticket!
+      assign_recipients!
+      trigger_notifications
+      success?
+    rescue ActiveRecord::RecordNotFound, ActiveRecord::RecordInvalid, ArgumentError => e
       @errors = e.message
       false
     end
@@ -34,7 +40,7 @@ module Tickets
     attr_reader :project, :assignee_ids
 
     def set_assignee_ids
-      if params[:assignees] != 'none'
+      if params[:assignees].present?
         params[:assignees]
           .split(',')
           .map { |id| id.strip.to_i }
@@ -54,20 +60,40 @@ module Tickets
     end
 
     def create_ticket!
-      @ticket = @project.tickets.new(
-        user: user,
+      @ticket = user.tickets.new(
+        project: project,
         title: params[:title].presence,
         description: params[:description].presence,
         status: params[:status].presence,
         priority: params[:priority].presence
       )
+
       @ticket.save!
+    end
+
+    def update_ticket!
+      attrs = {
+        title: params[:title].presence,
+        description: params[:description].presence,
+        status: params[:status].presence,
+        priority: params[:priority].presence
+      }
+
+      ticket.update!(attrs)
     end
 
     def assign_recipients!
       assignee_ids.each do |assignee_id|
         assignee = User.find_by!(id: assignee_id)
-        TicketAssignee.create!(ticket: ticket, user_id: assignee.id)
+        if !ticket.assignees.include?(assignee)
+          TicketAssignee.create!(ticket: ticket, user_id: assignee.id)
+        end
+      end
+    end
+
+    def trigger_notifications
+      if success? && ticket.persisted?
+        ::Notifications::TicketAssignedJob.perform_later(ticket_id: ticket.id)
       end
     end
 
